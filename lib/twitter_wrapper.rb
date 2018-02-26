5079de00f2f944b3556ecf3e41071b3323467115
chrislabarge <chrislabarge2@gmail.com>
2018-02-26 10:17:57 -0500

    add some logging

--- lib/twitter_wrapper.rb
+++ lib/twitter_wrapper.rb
@@ -8,6 +8,7 @@ module TwitterWrapper
     end
 
     def follow_creators_of_tweets_containing(text, limit = 80)
+      load_initial_counts
       terms = load_terms(text)
 
       limit_per_term = limit / terms.count
@@ -17,6 +18,8 @@ module TwitterWrapper
         max_tweets = tweets[0..limit_per_term]
         follow_users_from(max_tweets)
       end
+
+      log_success(terms)
     end
 
     def load_terms(text)
@@ -33,7 +36,7 @@ module TwitterWrapper
     def follow_users_from(nodes)
       users = nodes.map(&:user)
 
-      @client.follow!(users)
+      @client.follow(users)
     end
 
     def gather_tweets_containing(content)
@@ -47,6 +50,21 @@ module TwitterWrapper
         config.access_token        = ENV['TWITTER_TOKEN']
         config.access_token_secret = ENV['TWITTER_SECRET']
       end
+    end
+
+    def load_initial_counts
+      @was_following = @client.friend_ids.count
+    end
+
+    def load_final_counts
+      @is_following = @client.friend_ids.count
+    end
+
+    def log_success(terms)
+      load_final_counts
+      count = @is_following - @was_following
+      puts 'Success!!'
+      puts "Started following #{count} from the search terms #{terms}"
     end
   end
 end
